24008e2dc65726fe29fcf7a81f64fb3d7ca1697d
MunaRai <raimuna145@gmail.com>
2019-02-05 20:07:42 +0545

    script file is modified

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-/********************
- * variable mutation and type coercion
- */
-var firstName = 'John';
-var age = 28;;
-console.log(firstName +' '+ age);
+//logical Operator
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
 
-var job, isMarried;
-job = 'teacher';
-isMarried = 'false';
-console.log(firstName +' is a ' + age + ' year old ' + job + ' Is he married ? ' + isMarried);
-
-//variable mutation
-age = 'Twenty eight';
-job = 'Driver';
-alert(firstName +' is a ' + age + ' year old ' + job + ' Is he married ? ' + isMarried);
-
-var lastName = prompt("What is his lastname ? ");
-console.log(firstName + ' ' + lastName);
+//typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
