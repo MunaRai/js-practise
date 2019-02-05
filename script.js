4e7883b226763d5420413ade5f9bd74b1ca44f23
Mishal <raimuna145@gmail.com>
2019-02-05 19:28:38 +0545

    js file is modfied for lession 2

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*****************
-* variables and data types
-*/
-/*
-var firstName = 'Muna';
-console.log(firstName);
-var lastName = 'Rai';
-var age = 28;
-
-var fullAge = true;
-console.log(fullAge);
-
-var job;
-console.log(job)
-
-job = 'Teacher';
-console.log(job);
-
-//variable naming rules
-var _3years = 3;
-var johnMark = 'John and Mark';
-var if = 23;
-*/
-
 /********************
  * variable mutation and type coercion
  */
