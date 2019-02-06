fbf659f40ed6476d2cdcfbb00c6cb8cae6f46838
Mishal <raimuna145@gmail.com>
2019-02-06 17:46:23 +0545

    terner_switchcase modified

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-/*************************
- * Ternery Operator
- */
+//var drink = age >= 18 ? 'beer' : 'juice');
+//console.log(drink);
 
-var firstName = 'John';
-var age = 16;
+//Switch statement
 
-age >= 18 ? console.log(firstName + ' drinks beer. ') : console.log(firstName + ' drinks juice. ');
+var firstName = 'john';
+var job = "teacher";
+switch(job){
+    case 'teacher':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives in uber in lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' design beautiful websites.');
+        break;
+        default:
+        console.log(firstName + ' does something else.');
+}
