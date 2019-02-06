7e999748fdad9ba99aa50034bc05158ef8658373
Mishal <raimuna145@gmail.com>
2019-02-06 17:55:41 +0545

    ternery_switchcase again modified.

--- script.js
+++ script.js
@@ -15,6 +15,21 @@ switch(job){
     case 'designer':
         console.log(firstName + ' design beautiful websites.');
         break;
-        default:
+    default:
         console.log(firstName + ' does something else.');
 }
+
+var age = 10;
+switch(true){
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age > 13  && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >=20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man. ');
+} 
